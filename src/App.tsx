5fb5bd2ca11ffe4fdb9c5387a2a2c0d05b51806e
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-05-18 10:29:48 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [videoId, setVideoID] = useState<string>('')
+  const [isLoading, setisLoading] = useState(false)
+
+  async function handleSubmit() {
+    try {
+      setisLoading(true)
+      const trimmedID = videoId.trim()
+      if (!trimmedID) return;
+
+      const response = await fetch(`http://192.168.0.27:8000/download`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: trimmedID }),
+      });
+
+      const data = await response.json();
+
+
+    } catch (error) {
+      const errMsg = "Erro ao enviar comando: " + error;
+      alert(errMsg)
+
+    } finally {
+      setisLoading(false)
+
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Youtube Client</h1>
+      <div className="input-group">
+        <input
+          type='text'
+          placeholder='Insira o ID do vídeo'
+          value={videoId}
+          onChange={(e) => setVideoID(e.target.value)}
+        />
+        <button onClick={handleSubmit}></button>
+      </div>
     </div>
   );
 }

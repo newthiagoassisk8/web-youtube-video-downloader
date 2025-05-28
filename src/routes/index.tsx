83e8cbd338603f96292e2750361ab2e43f6171b9
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-05-28 20:38:07 -0300

    feat: update routing structure to use consistent path for login and improve formatting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route } from "react-router-dom";
 import { Login } from "../pages/Login/Login";
 import { Home } from "../pages/Home/Home";
@@ -8,24 +7,27 @@ import Scheduler from "../pages/Scheduler/Scheduler";
 import PrivateRoute from "./PrivateRoute";
 
 export const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<PrivateRoute>
-                <Home />
-            </PrivateRoute>} />
-            <Route path="/video-details" element={
-                <VideoDetails />
-
-            } />
-            <Route path="/history" element={
-                <PrivateRoute>
-                    <VideoHistory />
-                </PrivateRoute>
-
-
-            } />
-            <Route path="/schedule" element={<Scheduler />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/video-details" element={<VideoDetails />} />
+      <Route
+        path="/history"
+        element={
+          <PrivateRoute>
+            <VideoHistory />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/schedule" element={<Scheduler />} />
+    </Routes>
+  );
 };
